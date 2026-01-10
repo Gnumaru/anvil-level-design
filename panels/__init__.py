e4ddc91360ad8cd9ee23668a0edb7db34a445cf7
Alex Hetherington <alexjhetherington@gmail.com>
2026-01-10 20:50:55 +0000

    Remove freelook panel (pointless)

--- panels/__init__.py
+++ panels/__init__.py
@@ -1,12 +1,9 @@
 from . import uv_panel
-from . import freelook_panel
 
 
 def register():
     uv_panel.register()
-    freelook_panel.register()
 
 
 def unregister():
-    freelook_panel.unregister()
     uv_panel.unregister()
